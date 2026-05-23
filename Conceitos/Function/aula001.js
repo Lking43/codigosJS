f53f3bfc193c472f0f24d7a4eee851003f7cbd5e
Lucas Marques <luucaas.marquesz@gmail.com>
2026-05-23 07:53:47 -0300

    Inserindo Functions e Atualizando codigos

---
+++ Conceitos/Function/aula001.js
@@ -0,0 +1,26 @@
+/*
+SINTAXE BASICA DE FUNÇÕES
+
+
+function minhaprimeiraFunção() {
+    console.log()
+}
+
+
+minhaprimeiraFunção()
+
+*/
+
+/* EXEMPLO DE UMA FUNÇÃO 
+function minhaprimeiraFunção(name) {
+  console.log("meu nome é:" + name);
+}
+
+minhaprimeiraFunção("João");
+minhaprimeiraFunção("Maria");*/
+
+function quadrado(valor) {
+  return valor + valor;
+}
+quadrado(10);
+quadrado(5);
